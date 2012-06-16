e0e66ea8a2b41c02308cb68c1fa872a65c4751ca
Giovanni Cappellotto <potomak84@gmail.com>
2012-06-16 13:01:31 +0200

    Reindent code; minor changes

--- from_stadium.js
+++ from_stadium.js
@@ -5,19 +5,19 @@ function searchTwitter(query) {
     dataType: 'jsonp',
     success: function(data) {
       for (res in data['results']) {
-	created_at = data['results'][res]['created_at'].split(',')
-	time = created_at[1];
-	time = time.substring(1,18);
-	if (typeof timed_tweets[time] == 'undefined') {
-	  timed_tweets[time] = [data['results'][res]['from_user'] + ' : <p>' + data['results'][res]['text'] + '</p>']
-	}
-	else {
-	  timed_tweets[time].push(data['results'][res]['from_user'] + ' : <p>' + data['results'][res]['text'] + '</p>')
-	}
-      }
-      $(document).trigger('tweets-parsed');
-    }
-  });
+       created_at = data['results'][res]['created_at'].split(',')
+       time = created_at[1];
+       time = time.substring(1,18);
+       if (typeof timed_tweets[time] == 'undefined') {
+         timed_tweets[time] = [data['results'][res]['from_user'] + ' : <p>' + data['results'][res]['text'] + '</p>']
+       }
+       else {
+         timed_tweets[time].push(data['results'][res]['from_user'] + ' : <p>' + data['results'][res]['text'] + '</p>')
+       }
+     }
+     $(document).trigger('tweets-parsed');
+   }
+ });
 }
 
 function setTime(time) {
@@ -31,21 +31,27 @@ function setTime(time) {
 
 $(document).ready(function() {
   var clientId = '3365ff268278435aa79452ab1f3f80ca';
+  var stadiumCoords = [50.43222, 30.51803]
 
   $(".instagram.search").instagram({
     search: {
-      lat: 50.43222,
-      lng: 30.51803
+      lat: stadiumCoords[0],
+      lng: stadiumCoords[1],
+      min_timestamp: 1339793100,
+      max_timestamp: 1339800600
     },
-    clientId: clientId
+    clientId: clientId,
+    onComplete: function(data, res) {
+      console.log(data);
+      console.log(res);
+    }
   });
 
   $("#slider").slider({
     create: function(event, ui) {
       MATCH_DATA.forEach(function(match_event) {
-	console.log(match_event);
-	$("#slider").append("<div class='event " + match_event['type'] + "' style='left: " + ((match_event['time']/5400.0)*100) + "%'>" + match_event['type'] + " " + match_event['subjects'][0] + " (" + match_event['team'] + ")</div>")
-      });
+       $("#slider").append("<div class='event " + match_event['type'] + "' style='left: " + ((match_event['time']/5400.0)*100) + "%'>" + match_event['type'] + " " + match_event['subjects'][0] + " (" + match_event['team'] + ")</div>")
+     });
     }
   });
 
@@ -53,7 +59,7 @@ $(document).ready(function() {
     rpp: 100,
     since_id: 213725087092842497,
     max_id: 213763184979484672,
-    geocode: '50.43222,30.51803,1km'
+    geocode: stadiumCoords[0] + ',' + stadiumCoords[1] + ',1km'
   }
   searchTwitter(twitter_params);
 });
